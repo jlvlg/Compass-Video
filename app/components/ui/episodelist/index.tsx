94f6a335f99404f9b7c66924d15fe42ebbf3b43a
william <williambatista958@gmail.com>
2023-11-04 20:50:14 -0300

    refactor: ajustando lista de episodios

--- app/components/ui/episodelist/index.tsx
+++ app/components/ui/episodelist/index.tsx
@@ -1,21 +1,23 @@
 import tmdb from "@/util/tmdb";
 import Episode from "../episode";
+import styles from "./Episodelist.module.scss";
 
 type Props = {
-    id_serie: number;
-}
+  id_serie: number;
+};
 
 export default async function Episodelist(props: Props) {
-    const data = await tmdb.getSeasonInfo(props.id_serie, 1);
-    return (
-        <div>
-            <h2>Episodes</h2>
-            {data && data.episodes.length > 0 && 
-            data.episodes.map((item, index) => (
-                <Episode key={index} episode={item}/>
-            ))}
-        </div>
-    );
+  const data = await tmdb.getSeasonInfo(props.id_serie, 1);
+  return (
+    <div className={styles.maincontent}>
+      <h2>Episodes</h2>
+      <div className={styles.episodelist}>
+        {data &&
+          data.episodes.length > 0 &&
+          data.episodes.map((item, index) => (
+            <Episode key={index} episode={item} />
+          ))}
+      </div>
+    </div>
+  );
 }
-
-
