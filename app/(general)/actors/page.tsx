ce4f5859073bdc74159b370efaaa911aac56fda5
William Santos <williambatista958@gmail.com>
2023-11-08 02:43:54 -0300

    feat: criando tela de atores

--- app/(general)/actors/page.tsx
+++ app/(general)/actors/page.tsx
@@ -1,20 +1,48 @@
-"use client";
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 import Header from "@/app/components/header";
 import styles from "./Actors.module.scss";
 import Carousel from "@/app/components/carousel";
 import tmdb from "@/util/tmdb";
-export default async function page() {
+import { Media } from "@/util/model";
+const defaultImagePoster = "/carouseldefault.png";
+const randomNumber = Math.floor(Math.random() * 19);
+
+export default async function ActorCredits() {
   const actors = await tmdb.getPeopleList();
-  const itemheader = await tmdb.detailedMedia(actors[0].known_for[0]);
+  const firstActor = await tmdb.getCreditsActor(actors[0].id);
+
+  let media;
+  const actorMedias: Media[][] = [];
+  for (let i = 0; i < actors.length; i++) {
+    media = await tmdb.getCreditsActor(actors[i].id);
+    actorMedias.push(media);
+  }
+
+
+  function getImage(path: string) {
+    if (path) {
+      return `https://image.tmdb.org/t/p/w342${path}`;
+    }
+    return defaultImagePoster;
+  }
 
   return (
-    <div>
-      <Header item={itemheader} autoUpdate />
-      <div className={styles.content}>
-        <img src="https://image.tmdb.org/t/p/w200/9jkThAGYj2yp8jsS6Nriy5mzKFT.jpg" />
-
-       {actors[0] &&  <Carousel items={actors[0].known_for} />}
-       </div>
+    <div className={styles.maincontent}>
+      <Header item={await tmdb.detailedMedia(firstActor[randomNumber])} autoUpdate buttons={["watch", "info", "controls"]}/>
+      
+      {actorMedias.map((item: Media[], index) => (
+        <div key={index} className={styles.content}>
+          <Carousel items={item} title="Conhecido(a) por"/>
+          <div className={styles.actor}>
+            <h2>{actors[index].name}</h2>
+            <img
+              src={getImage(actors[index].profile_path)}
+              className={styles.actorimage}
+            />
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
